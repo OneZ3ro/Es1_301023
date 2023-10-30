d0e3cc3f7230b5b6800aa4acada5460e27d8ded2
Angelo Moreno <emilioangelo665@gmail.com>
2023-10-30 13:49:14 +0100

    Creato progetto con springboot

---
+++ src/main/java/angelomoreno/Es1_301023/Es1301023Application.java
@@ -0,0 +1,13 @@
+package angelomoreno.Es1_301023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es1301023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es1301023Application.class, args);
+	}
+
+}
---
+++ src/test/java/angelomoreno/Es1_301023/Es1301023ApplicationTests.java
@@ -0,0 +1,13 @@
+package angelomoreno.Es1_301023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es1301023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
