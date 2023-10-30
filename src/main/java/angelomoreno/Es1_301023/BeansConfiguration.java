815284982532399f16339ac1bd10ec98cc90c7dd
Angelo Moreno <emilioangelo665@gmail.com>
2023-10-30 16:07:09 +0100

    create classi e main

---
+++ src/main/java/angelomoreno/Es1_301023/BeansConfiguration.java
@@ -0,0 +1,92 @@
+package angelomoreno.Es1_301023;
+
+import angelomoreno.Es1_301023.entities.Drink;
+import angelomoreno.Es1_301023.entities.Pizza;
+import angelomoreno.Es1_301023.entities.Topping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class BeansConfiguration {
+    @Bean
+    @Scope("prototype")
+    Pizza margherita() {
+        List<String> ingredienti = new ArrayList<>();
+        ingredienti.add("pomodoro");
+        ingredienti.add("mozzarella");
+        return new Pizza("margherita", 1104, 4.99, ingredienti);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Pizza odiolitalia() {
+        List<String> ingredienti = new ArrayList<>();
+        ingredienti.add("pomodoro");
+        ingredienti.add("mozzarella");
+        ingredienti.add("prosciutto");
+        ingredienti.add("ananas");
+        return new Pizza("odiolitalia", 1024, 6.49, ingredienti);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Pizza salume() {
+        List<String> ingredienti = new ArrayList<>();
+        ingredienti.add("pomodoro");
+        ingredienti.add("mozzarella");
+        ingredienti.add("salame");
+        return new Pizza("salume", 1160, 5.99, ingredienti);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Topping formaggio() {
+        return new Topping("formaggio", 92, 0.69);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Topping prosciutto() {
+        return new Topping("prosciutto", 35, 0.99);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Topping cipolla() {
+        return new Topping("cipolla", 22, 0.69);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Topping pineapple() {
+        return new Topping("pineapple", 24, 0.79);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Topping salami() {
+        return new Topping("salami", 86, 0.99);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Drink limonata() {
+        return new Drink("limonata", 128, 1.29, 0.33);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Drink acqua() {
+        return new Drink("acqua", 0, 1.29, 0.5);
+    }
+
+    @Bean
+    @Scope("prototype")
+    Drink vino() {
+        return new Drink("vino", 607, 7.49, 0.75, 13);
+    }
+}
--- src/main/java/angelomoreno/Es1_301023/Es1301023Application.java
+++ src/main/java/angelomoreno/Es1_301023/Es1301023Application.java
@@ -2,12 +2,35 @@ package angelomoreno.Es1_301023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Es1301023Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Es1301023Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Es1301023Application.class, args);
+        configurationClass();
+    }
 
+    public static void configurationClass() {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Es1301023Application.class);
+        System.out.println("\n---------- PIZZE ---------- PIZZE ---------- PIZZE ---------- PIZZE ---------- PIZZE ---------- PIZZE ----------");
+        System.out.println(ctx.getBean("margherita"));
+        System.out.println(ctx.getBean("odiolitalia"));
+        System.out.println(ctx.getBean("salume"));
+        System.out.println();
+        System.out.println("---------- AGGIUNTE ---------- AGGIUNTE ---------- AGGIUNTE ---------- AGGIUNTE ---------- AGGIUNTE ----------");
+        System.out.println(ctx.getBean("formaggio"));
+        System.out.println(ctx.getBean("prosciutto"));
+        System.out.println(ctx.getBean("cipolla"));
+        System.out.println(ctx.getBean("pineapple"));
+        System.out.println(ctx.getBean("salami"));
+        System.out.println();
+        System.out.println("---------- BEVANDE ---------- BEVANDE ---------- BEVANDE ---------- BEVANDE ---------- BEVANDE ----------");
+        System.out.println(ctx.getBean("limonata"));
+        System.out.println(ctx.getBean("acqua"));
+        System.out.println(ctx.getBean("vino"));
+
+        ctx.close();
+    }
 }
